4841ad90187d871c15dca0a78fe99f29bdad0dc3
esentai <esentaik@gmail.com>
2018-08-19 13:28:34 +0600

    First commit

---
+++ src/ChildMarker.vue
@@ -0,0 +1,34 @@
+<template></template>
+<script>
+export default {
+  inject: ["google", "map"],
+  props: {
+    position: Object,
+  },
+  data(){
+    return {
+      marker: null,
+
+    }
+  },
+  mounted(){
+    const { Marker } = this.google.maps;
+    this.marker = new Marker({
+      position: {
+        lat: parseFloat(this.position.lat),
+        lng: parseFloat(this.position.lng),
+      },
+      icon:{
+        url: this.position.icon,
+        scaledSize: new google.maps.Size(40, 40)
+      },
+      map: this.map,
+      title: "Child marker!",
+    });
+    this.marker.addListener( 'click', function() {
+      console.log("Salem Alem");
+    });
+  }
+}
+</script>
+
---
+++ src/MyMap.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-9" style="padding-left: 0">
+        <map-loader
+          :map-config="mapConfig"
+          apiKey="AIzaSyCdsCZwgVbKBnvwqtPQ8Mqcv5P0lNzuxs8"
+        >
+          <template v-for="marker in markers">
+            <child-marker
+              :position="marker"
+            />
+          </template>
+        </map-loader>
+      </div>
+      <div class="col-md-3">
+        <h3>{{this.name}}</h3>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MapLoader from "./MapLoader.vue"
+import ChildMarker from './ChildMarker'
+
+export default {
+  props: {
+    markers: Array
+  },
+
+  data(){
+    return {
+      name: this.name,
+      mapConfig: {
+        zoom: 17,
+        center: this.markers[0],
+        zoomControl: true,
+        mapTypeControl: false,
+        scaleControl: true,
+        streetViewControl: true,
+        rotateControl: false,
+        fullscreenControl: false
+      }
+    }
+  },
+  components: {
+
+    MapLoader,
+    ChildMarker,
+  }
+}
+</script>
+
